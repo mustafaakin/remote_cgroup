04eb302b38221c46a2c223f1caefb97a3fe91726
Mustafa <you@example.com>
2015-04-25 02:07:30 +0300

    device major/minor finder

--- main.go
+++ main.go
@@ -2,8 +2,44 @@ package main
 
 import (
     "fmt"
+    "io/ioutil"
+    "syscall"
 )
+
+func check(e error) {
+    if e != nil {
+        panic(e)
+    }
+}
+
+type Device struct {
+    name string
+    minor int
+    major int
+}
+
+func (d Device) String() string{
+    return fmt.Sprintf("Name: %s major: %d minor: %d", d.name, d.major, d.minor)
+}
 
 func main() {
     fmt.Println("Hello World")
+
+    files, err := ioutil.ReadDir("/dev")
+    check(err)
+
+    for _, file := range files {
+        Sys := file.Sys()
+
+        if  Sys != nil  {
+            stat := Sys.(*syscall.Stat_t)
+            Rdev := stat.Rdev
+
+            minor := 0xFF & Rdev // get first 8 bits only
+            major := (Rdev - minor) >> 8 // subtracting minor just in case
+
+            dev := Device{name: file.Name(), minor: int(minor), major: int(major)}
+            fmt.Printf("%s \n", dev)
+        }
+    }
 }
